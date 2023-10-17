0fa462e8ae6e371fa671d85ff57645a1b3803bf8
Ashley Trinh <itsashley@hey.com>
2023-10-17 11:55:11 -0700

    themes: rename template files

--- src/sphinx_revealjs_slides/builder.py
+++ src/sphinx_revealjs_slides/builder.py
@@ -1,6 +1,11 @@
 """sphinx_revealjs_slides.builder"""
 
+from typing import TYPE_CHECKING
+
 from sphinx.builders.html import StandaloneHTMLBuilder
+
+if TYPE_CHECKING:
+    from typing import Any
 
 
 class RevealjsBuilder(StandaloneHTMLBuilder):
@@ -9,3 +14,13 @@ class RevealjsBuilder(StandaloneHTMLBuilder):
 
     def get_theme_config(self) -> tuple[str, dict]:
         return self.config.revealjs_html_theme, self.config.revealjs_html_theme_options
+
+    def handle_page(
+        self,
+        pagename: str,
+        addctx: dict,
+        templatename: str = "slides.html",
+        outfilename: str | None = None,
+        event_arg: "Any" = None,
+    ) -> None:
+        super().handle_page(pagename, addctx, templatename, outfilename, event_arg)
